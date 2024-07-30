d1cebfdaf6070f7459dfd5b85f70c18d339e1a02
Norma Granados <paraisoocho@gmail.com>
2024-07-30 09:08:22 -0600

    initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
+import { ThemeContextProvider } from '@/context/ThemeContext';
+import ThemeProvider from '@/providers/ThemeProvider';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -13,12 +15,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <ThemeContextProvider>
+          <ThemeProvider>
         <div className="container">
           <div className='wrapper'><Navbar/>
           {children}
           <Footer/>
           </div>
         </div>
+        </ThemeProvider>
+        </ThemeContextProvider>
         </body>
     </html>
   )
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import Image from "next/image"
 import styles from "./featured.module.css"
 const Featured=()=>{
     return(
-        <div className={styles.container}>Featured</div>
+        <div className={styles.container}>
+            <h1 className={styles.title}><b>¡Hey, Patricia here!</b> Discover my stories and creative ideas. </h1>
+        <div className={styles.post}>
+            <div className={styles.imgContainter}>
+                <Image src="/p1.jpeg" alt="" fill/>
+            </div>
+            <div className={styles.textContainter}>
+                <h1 className={styles.postTitle}>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                <p className={styles.postDescription}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut ipsam exercitationem, sequi temporibus ad dolorum corrupti quisquam delectus.</p>
+                <button>Read more</button>
+            </div>
+
+        </div>
+        </div>
     )
 }
 export default Featured
