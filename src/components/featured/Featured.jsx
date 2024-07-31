c31b5fdcd45955e8d2e5fed32cc017b4f2acbd76
Norma Granados <paraisoocho@gmail.com>
2024-07-31 11:56:20 -0600

    create featured

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import Image from "next/image"
-import styles from "./featured.module.css"
+import Image from "next/image";
+import styles from "./featured.module.css";
 const Featured=()=>{
     return(
         <div className={styles.container}>
             <h1 className={styles.title}><b>¡Hey, Patricia here!</b> Discover my stories and creative ideas. </h1>
         <div className={styles.post}>
             <div className={styles.imgContainter}>
-                <Image src="/p1.jpeg" alt="" fill/>
+                <Image src="/p1.jpeg" alt="" width={450} height={400} className={styles.image}/>
             </div>
             <div className={styles.textContainter}>
                 <h1 className={styles.postTitle}>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h1>
                 <p className={styles.postDescription}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut ipsam exercitationem, sequi temporibus ad dolorum corrupti quisquam delectus.</p>
-                <button>Read more</button>
+                <button className={styles.button}>Read more</button>
             </div>
 
         </div>
