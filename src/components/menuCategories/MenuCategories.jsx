c7cd637a1e9364344cfc4f0821302c374f4da648
Norma Granados <paraisoocho@gmail.com>
2024-08-08 11:06:44 -0600

    create menu categories

---
+++ src/components/menuCategories/MenuCategories.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from 'next/link'
+import styles from './menuCategories.module.css'
+
+const MenuCategories = () => {
+  return (
+    <div>
+      <div className={styles.categoryList}>
+                <Link href="blog?cat=style" className={`${styles.categoryItem} ${styles.style}`}>Style</Link>
+                <Link href="blog?cat=style" className={`${styles.categoryItem} ${styles.fashion}`}>Fashion</Link>
+                <Link href="blog?cat=style" className={`${styles.categoryItem} ${styles.food}`}>Food</Link>
+                <Link href="blog?cat=style" className={`${styles.categoryItem} ${styles.travel}`}>Travel</Link>
+                <Link href="blog?cat=style" className={`${styles.categoryItem} ${styles.coding}`}>Coding</Link>
+                <Link href="blog?cat=style" className={`${styles.categoryItem} ${styles.culture}`}>Culture</Link>
+            </div>
+    </div>
+  )
+}
+
+export default MenuCategories
